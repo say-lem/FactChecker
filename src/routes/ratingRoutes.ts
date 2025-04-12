7f1611bef58bcdf816f6f46ef35e97923733c717
say-lem <nedusalemonah@gmail.com>
2025-04-12 15:46:41 +0100

    getQueryById route created

--- src/routes/ratingRoutes.ts
+++ src/routes/ratingRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { submitRating } from '../controllers/ratingController';
+import { submitRating, updateRating } from '../controllers/ratingController';
 import {  authenticate } from '../middlewares/auth'; 
 
 const router = express.Router();
 
 router.post('/api/queries/:id/rate',  authenticate, submitRating);
+router.put('/api/queries/:id/rate', authenticate, updateRating);
 
 export default router;
